60265abdcdd6592aeabf25b7df2b88340039584c
VAGASTEC\rrodrigues.7comm <renatoj.rodrigues@yahoo.com.br>
2019-02-19 11:47:03 -0300

    Updates

---
+++ precache-manifest.879091ba2cdcd3edb61286d1b4c3e88b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chars-creator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7cd9907efefff48d6324",
+    "url": "/chars-creator/static/js/runtime~main.7cd9907e.js"
+  },
+  {
+    "revision": "f6bb9d2cd95e41c4f787",
+    "url": "/chars-creator/static/js/main.f6bb9d2c.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/chars-creator/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "f6bb9d2cd95e41c4f787",
+    "url": "/chars-creator/static/css/main.2f85f37e.chunk.css"
+  },
+  {
+    "revision": "3f835974bc0b8215368cad5ed2e6ce00",
+    "url": "/chars-creator/index.html"
+  }
+];
